158b3e0c8e75bf340d0463c3f0cf892c8c69daab
Alexandr <schidlowski95@tut.by>
2019-09-13 10:26:33 +0300

    Package fix convert_laser_scan

--- src/convert_laser_scan/src/take_laser_scan.cpp
+++ src/convert_laser_scan/src/take_laser_scan.cpp
@@ -6,44 +6,51 @@
 
 laser_geometry::LaserProjection projector_;
 
+ros::Publisher pub;
+
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr scan_in){
-    
     sensor_msgs::PointCloud2 cloud2;
     projector_.projectLaser(*scan_in,cloud2);
 
-    ros::NodeHandle nh;
-    ros::Rate rate(1);
-
-    std::string output_point_cloud2_topic;
-    ros::param::get("/take_laser_scan/output_point_cloud2_topic",output_point_cloud2_topic);
-    ROS_INFO_STREAM("Name of output topic PointCloud2 data: [ "<<output_point_cloud2_topic<<" ]");
-
-    //public to topic "point_cloud2_data" data after conversion LaserScan to PointCloud2
-    ros::Publisher pub=nh.advertise<sensor_msgs::PointCloud2>(output_point_cloud2_topic,100);
-
-    while(ros::ok()){
-        //data will already be filled
-        pub.publish(cloud2);
-        rate.sleep();
-    }
-  
+    pub.publish(cloud2);
 }
 
-
 int main(int argc, char **argv){
+
     ros::init(argc, argv, "take_laser_scan");
 
-    ros::NodeHandle nh;
-    std::string input_laser_scan_topic;
-    ros::param::get("/take_laser_scan/input_laser_scan_topic",input_laser_scan_topic);
-    ROS_INFO_STREAM("Name of input topic LaserScan data:[ "<<input_laser_scan_topic<<" ]");
+    ros::NodeHandle privateNodeHandle("~");
+
+    std::string input_topic;
+    if(!privateNodeHandle.getParam("input_topic", input_topic)){
+        ROS_ERROR_STREAM("Error getParam input_topic");
+        return 1;
+    }
+    else
+    {
+        ROS_INFO_STREAM("Name of input topic LaserScan data:[ "<<input_topic<<" ]");
+    }
     
+    std::string output_topic;
+    if (!privateNodeHandle.getParam("output_topic", output_topic)) {
+        ROS_ERROR_STREAM("Error getParam output_topic");
+        return 1;
+    }
+    else
+    {
+        ROS_INFO_STREAM("Name of output topic PointCloud2 data: [ "<<output_topic<<" ]");
+    }
+    
+    ros::NodeHandle nh;
+
+    //public to topic "point_cloud2_data" data after conversion LaserScan to PointCloud2
+    pub=nh.advertise<sensor_msgs::PointCloud2>(output_topic,100);
+
     ROS_INFO_STREAM("Start take_laser_scan node");
 
     //subscribed to the topic with the data LaserScan
-    ros::Subscriber sub=nh.subscribe(input_laser_scan_topic,100,scanCallback);
-    
+    ros::Subscriber sub=nh.subscribe(input_topic,100,scanCallback);
     
     ros::spin();
 
